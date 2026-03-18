ad2abc2433d42c74229ef96c5f0dfa79d7973958
Toostew <tookaystewie@gmail.com>
2026-03-18 11:53:39 +0800

    initial commit

---
+++ src/test/java/com/toostew/file_scanner/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toostew.file_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
